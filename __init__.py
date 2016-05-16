1cd269e6dab78fd25ed832fdc96e61b370c9a448
jcallin <j.callin@hotmail.com>
2016-05-15 22:22:59 -0700

    Added favicon

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 # Route the home directory of our webpage
 @app.route('/')
 def index():
-    return 'This is the homepage'
+    return render_template("main.html")
 
 # Only start the webserver if this file is called directly
 if __name__ == "__main__":
