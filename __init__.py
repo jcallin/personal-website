ad2425e21beecc9bbc863cbd5b1d2e99ebf68963
jcallin <j.callin@hotmail.com>
2016-10-13 19:39:05 -0700

    Added content service and modularized python embedded in HTML

--- __init__.py
+++ __init__.py
@@ -1,11 +1,17 @@
 from flask import Flask, request, render_template
+from content import Content
 
+content_dict = Content()
 app = Flask(__name__)
 
 # Route the home directory of our webpage
 @app.route('/')
 def index():
-    return render_template("main.html")
+    return render_template("main.html", content_dict = content_dict)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return
 
 # Only start the webserver if this file is called directly
 if __name__ == "__main__":
---
+++ content.py
@@ -0,0 +1,15 @@
+def Content():
+    dict = {"pictures":[
+            {"broncohack":[
+
+                {"id" : "broncohack1",
+                "src" : "static/bronco_hack1.jpg",
+                "alt" : "Bronco Hack 2016"},
+
+                {"id" : "broncohack2",
+                "src" : "static/bronco_hack2.jpg",
+                "alt" : "Bronco Hack 2016"}
+
+            ]}]}
+
+    return dict
